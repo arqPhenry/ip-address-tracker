1e1a9c9e72b5e4081aac10b8a9af732c7e3fe3a3
Henry Navas <navashenrym@gmail.com>
2024-03-01 01:48:14 -0300

    [ALIVE] MVP desplegado

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+const map = L.map('map', {zoomControl: false});
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  minZoom: 13,
+  maxZoom: 19,
+  reuseTiles: true, 
+  unloadInvisibleTiles: true,
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+
 async function getIPAddress() {
     try {
       const response = await fetch('https://geo.ipify.org/api/v2/country,city?apiKey=at_qAYSlGbMocnyVBCQYLa1hE0EofQ60');
@@ -13,13 +24,19 @@ async function getIPAddress() {
       inputIp.value = data.ip;
       ipText.innerText = data.ip;
       cityText.innerText = `${data.location.city}, ${data.location.region} ${data.location.postalCode}`;
-      timezoneText.innerText = data.location.timezone;
+      timezoneText.innerText = `UTC ${data.location.timezone}` ;
       ispText.innerText = data.isp;
+
+      map.setView([data.location.lat, data.location.lng], 13);
+
+      const marker = L.marker([data.location.lat, data.location.lng]).addTo(map);
+      marker.bindPopup(`${data.location.lat}, ${data.location.lng}`);
+      marker.openPopup();
 
     } catch (error) {
       console.error('Error al obtener la IP del usuario:', error);
     }
   }
 
-//window.onload(getIPAddress());
+window.onload(getIPAddress());
 
