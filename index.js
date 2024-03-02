d3a293db0ed331589394104ba6676cc7dd697fe8
Henry Navas <navashenrym@gmail.com>
2024-03-02 00:54:13 -0300

    [FINISHED] add function to button

--- index.js
+++ index.js
@@ -8,20 +8,39 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
+const inputIp = document.getElementById('input-ip');
+const ipText = document.getElementById('ip-text');
+const cityText = document.getElementById('city-text');
+const timezoneText = document.getElementById('timezone-text');
+const ispText = document.getElementById('isp-text');
+const primaryButton = document.getElementById('obtenerIPbutton');
 
-async function getIPAddress() {
+function esDireccionIP(entrada) {
+  const regexIP = /^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/;
+  return regexIP.test(entrada);
+}
+
+function esDominio(entrada) {
+  const regexDominio = /^[a-zA-Z0-9][a-zA-Z0-9-]*\.[a-zA-Z]{2,}$/;
+  return regexDominio.test(entrada);
+}
+
+async function getIPAddress(parameter) {
+    let url;
+
+    if (esDireccionIP(parameter)){
+      url = `https://geo.ipify.org/api/v2/country,city?apiKey=at_qAYSlGbMocnyVBCQYLa1hE0EofQ60&ipAddress=${parameter}`;
+    } else if (esDominio(parameter)){
+      url = `https://geo.ipify.org/api/v2/country,city?apiKey=at_qAYSlGbMocnyVBCQYLa1hE0EofQ60&domain=${parameter}`;
+    } else{
+      url = 'https://geo.ipify.org/api/v2/country,city?apiKey=at_qAYSlGbMocnyVBCQYLa1hE0EofQ60';
+    }
+
     try {
-      const response = await fetch('https://geo.ipify.org/api/v2/country,city?apiKey=at_qAYSlGbMocnyVBCQYLa1hE0EofQ60');
+      const response = await fetch(`${url}`);
       
       const data = await response.json();
-      
-      const inputIp = document.getElementById('input-ip');
-      const ipText = document.getElementById('ip-text');
-      const cityText = document.getElementById('city-text');
-      const timezoneText = document.getElementById('timezone-text');
-      const ispText = document.getElementById('isp-text');
 
-      inputIp.value = data.ip;
       ipText.innerText = data.ip;
       cityText.innerText = `${data.location.city}, ${data.location.region} ${data.location.postalCode}`;
       timezoneText.innerText = `UTC ${data.location.timezone}` ;
@@ -34,9 +53,11 @@ async function getIPAddress() {
       marker.openPopup();
 
     } catch (error) {
-      console.error('Error al obtener la IP del usuario:', error);
+      console.error('Error al obtener la IP o Dominio buscado:', error);
     }
   }
+
+primaryButton.addEventListener('click', () => getIPAddress(inputIp.value));
 
 window.onload(getIPAddress());
 
